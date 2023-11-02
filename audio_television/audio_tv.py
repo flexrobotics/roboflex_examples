3287a890aa5eb77194f336ac9d83559dc39d5e96
Colin Prepscius <colinprepscius@gmail.com>
2023-11-01 22:03:46 -0400

    reworked audio_tv example

--- audio_television/audio_tv.py
+++ audio_television/audio_tv.py
@@ -1,17 +1,17 @@
-import time
-import roboflex as rcc
 import roboflex.audio_sdl as ras
 import roboflex.imgui as rgu
 
+ras.AudioSensor.show_devices()
+
 sensor = ras.AudioSensor(
-    capture_id = -1,
+    capture_id = 1,
     channels = 1,
     sampling_rate = 44100,
-    capture_samples = 512,
+    capture_samples = 1024,
     format = ras.BitDepth.F32,
     name = "mysensor",
     data_key = "data",
-    debug = False,
+    debug = True,
 )
 
 viewer = rgu.OneDTV(
@@ -21,15 +21,14 @@ viewer = rgu.OneDTV(
     initial_size = (800, 120),
     initial_pos = (100, -1),
     name = "myviewer",
-    debug = False,
+    debug = True,
 )
 
 sensor > viewer
 
 sensor.start()
-viewer.start()
 
-time.sleep(10000)
+# will block until window closed
+viewer.run()
 
 sensor.stop()
-viewer.stop()
